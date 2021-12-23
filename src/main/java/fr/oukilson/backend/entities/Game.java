f14cafffb59901ae7f8afab0cb2a63dc4360c3c0
Didier <didier.dourneau@yahoo.com>
2021-12-23 11:19:35 +0100

    feat: [Game] creating entity

---
+++ src/main/java/fr/oukilson/backend/entities/Game.java
@@ -0,0 +1,27 @@
+package fr.oukilson.backend.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int minPlayer;
+    private int maxPlayer;
+    // Duration in minutes
+    private int minTime;
+    private int maxTime;
+    private int age;
+    private String creatorName;
+
+}
