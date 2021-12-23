94139045daae3049fb034ba14db8b55218c9bcc7
Didier <didier.dourneau@yahoo.com>
2021-12-23 14:07:59 +0100

    feat: [Game] General GameDTO added

--- src/main/java/fr/oukilson/backend/dtos/GameDTO.java
+++ src/main/java/fr/oukilson/backend/dtos/GameDTO.java
@@ -1,2 +1,19 @@
-package fr.oukilson.backend.dtos;public class GameDTO {
+package fr.oukilson.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameDTO {
+    private int id;
+    private String name;
+    private int minPlayer;
+    private int maxPlayer;
+    private int minTime;
+    private int maxTime;
+    private int age;
+    private String creatorName;
 }
