eec331eba233be89e139a55967942a10a78cefcc
clement <clement.cauliez@gmail.com>
2021-12-23 16:27:20 +0100

    test: [user] added service & controller tests

---
+++ src/test/java/fr/oukilson/backend/controller/UserControllerTest.java
@@ -0,0 +1,45 @@
+package fr.oukilson.backend.controller;
+
+import fr.oukilson.backend.dto.CreationResponseDTO;
+import fr.oukilson.backend.dto.UserCreationDTO;
+import com.google.gson.Gson;
+import fr.oukilson.backend.service.UserService;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    public UserCreationDTO userCreationDTO(){
+        return new UserCreationDTO("password", "email@email.com", "nickname");
+    }
+
+    public Gson gson(){
+        return new Gson();
+    }
+    @Test
+    public void testCreateUser() throws Exception{
+        UserCreationDTO userCreationDTO = userCreationDTO();
+        CreationResponseDTO creationResponseDTO = new CreationResponseDTO(true, "success");
+        given(userService.createUser(userCreationDTO)).willReturn(creationResponseDTO);
+        this.mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson().toJson(userCreationDTO)))
+                .andExpect(status().isCreated());
+    }
+}
--- src/test/java/fr/oukilson/backend/service/UserServiceTest.java
+++ src/test/java/fr/oukilson/backend/service/UserServiceTest.java
@@ -1,35 +1,72 @@
 package fr.oukilson.backend.service;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import fr.oukilson.backend.dto.CreationResponseDTO;
+import fr.oukilson.backend.dto.UserCreationDTO;
+import fr.oukilson.backend.entity.User;
+import fr.oukilson.backend.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.regex.Matcher;
+import java.util.List;
 import java.util.regex.Pattern;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(SpringRunner.class)
 public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @MockBean
+//    @InjectMocks
+    private UserService userService;
+
+
+    @Test
+    public void testCreateUser_AssertTrue(){
+        CreationResponseDTO creationResponseDTO = new CreationResponseDTO(true, "success");
+        UserCreationDTO userCreationDTO = new UserCreationDTO("password", "hello@example.com", "nickname");
+        when(userService.createUser(any(UserCreationDTO.class))).thenReturn(creationResponseDTO);
+        creationResponseDTO = userService.createUser(userCreationDTO);
+        Assertions.assertTrue(creationResponseDTO.isSuccess());
+    }
+
 
     @Test
     @DisplayName("testing email checking method")
     public void emailIsValidAssertTrue(){
+        String emailRegex = "(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))";
         String[] emails = {"jeanpierre@email.com", ".j@e.c", "jeanpierre@email.com.", "jeanpierre@yahoo.com"};
-        String emailRegex;
-//        Assertions.assertTrue(Pattern.compile(dotenv.get("EMAIL_REGEX")).matcher(emails[0]).find());
-//        Assertions.assertFalse(Pattern.compile(dotenv.get("EMAIL_REGEX")).matcher(emails[1]).find());
-//        Assertions.assertFalse(Pattern.compile(dotenv.get("EMAIL_REGEX")).matcher(emails[2]).find());
-//        Assertions.assertTrue(pattern.matcher("hello@example.com").find());
+//        Assertions.assertTrue(Pattern.compile(emailRegex).matcher(emails[0]).find());
+//        Assertions.assertFalse(Pattern.compile(emailRegex).matcher(emails[1]).find());
+//        Assertions.assertFalse(Pattern.compile(emailRegex).matcher(emails[2]).find());
+        Assertions.assertTrue(Pattern.compile(emailRegex).matcher("hello@example.com").find());
     }
 
     @Test
     @DisplayName("testing username validation method")
     public void nicknameIsValid(){
         String[] nicknames = {"titi", "t", "titi2", "..azd"};
-        Dotenv dotenv = Dotenv.load();
-//        Assertions.assertTrue(Pattern.compile(dotenv.get("NICKNAME_REGEX")).matcher(nicknames[0]).find());
-//        Assertions.assertFalse(Pattern.compile(dotenv.get("NICKNAME_REGEX")).matcher(nicknames[1]).find());
-//        Assertions.assertTrue(Pattern.compile(dotenv.get("NICKNAME_REGEX")).matcher(nicknames[2]).find());
-        Assertions.assertFalse(Pattern.compile(dotenv.get("NICKNAME_REGEX")).matcher(nicknames[3]).find());
+        String nicknameRegex = "^[a-zA-Z0-9_-]{4,16}$";
+//        Assertions.assertTrue(Pattern.compile(nicknameRegex).matcher(nicknames[0]).find());
+        Assertions.assertFalse(Pattern.compile(nicknameRegex).matcher(nicknames[1]).find());
+//        Assertions.assertTrue(Pattern.compile(nicknameRegex).matcher(nicknames[2]).find());
+//        Assertions.assertFalse(Pattern.compile(nicknameRegex).matcher(nicknames[3]).find());
     }
 }
