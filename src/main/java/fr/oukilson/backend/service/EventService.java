50e9c5a420741a03b689baf354a64a57ccea07e5
LMika <lemika.pro@gmail.com>
2021-12-23 15:32:40 +0100

    fix[User]: Event save, needed user's id and game's id

--- src/main/java/fr/oukilson/backend/service/EventService.java
+++ src/main/java/fr/oukilson/backend/service/EventService.java
@@ -4,15 +4,28 @@ import fr.oukilson.backend.dto.event.EventCreateDTO;
 import fr.oukilson.backend.dto.event.EventDTO;
 import fr.oukilson.backend.dto.event.EventUpdateDTO;
 import fr.oukilson.backend.entity.Event;
+import fr.oukilson.backend.entity.Game;
+import fr.oukilson.backend.entity.User;
 import fr.oukilson.backend.repository.EventRepository;
+import fr.oukilson.backend.repository.GameRepository;
+import fr.oukilson.backend.repository.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
 
 public class EventService {
     private EventRepository repository;
+    private UserRepository userRepository;
+    private GameRepository gameRepository;
     private ModelMapper mapper;
 
-    public EventService(EventRepository repository, ModelMapper mapper) {
+    public EventService(EventRepository repository, UserRepository userRepository, GameRepository gameRepository, ModelMapper mapper) {
         this.repository = repository;
+        this.userRepository = userRepository;
+        this.gameRepository = gameRepository;
         this.mapper = mapper;
     }
 
@@ -35,6 +48,7 @@ public class EventService {
      * Delete an event by its uuid
      * @param uuid Event's uuid
      */
+    @Transactional
     public void deleteByUuid(String uuid) {
         this.repository.deleteByUuid(uuid);
     }
@@ -44,8 +58,29 @@ public class EventService {
      * @param toCreate The event to add
      * @return The created event
      */
-    public EventCreateDTO save(EventCreateDTO toCreate) {
+    public EventCreateDTO save(EventCreateDTO toCreate) throws NoSuchElementException {
         Event event = this.mapper.map(toCreate, Event.class);
+        event.setUuid(UUID.randomUUID().toString());
+
+        // Get the user by its nickname
+        Optional<User> user = this.userRepository.findByNickname(toCreate.getCreator().getNickname());
+        try {
+            event.setCreator(user.get());
+        }
+        catch (Exception e) {
+            throw new NoSuchElementException("Event creation : Unknown user");
+        }
+
+        // Get the game by its uuid
+        Optional<Game> game = this.gameRepository.findByUuid(toCreate.getGame().getUuid());
+        try {
+            event.setGame(game.get());
+        }
+        catch (Exception e) {
+            throw new NoSuchElementException("Event creation : Unknown game");
+        }
+
+        // Save and return
         event = this.repository.save(event);
         return this.mapper.map(event, EventCreateDTO.class);
     }
@@ -55,6 +90,7 @@ public class EventService {
      * @param toUpdate The event to update
      * @return The updated event
      */
+    // TODO
     public EventUpdateDTO update(EventUpdateDTO toUpdate) {
         Event event = this.repository.findByUuid(toUpdate.getUuid()).orElse(null);
         EventUpdateDTO result = null;
