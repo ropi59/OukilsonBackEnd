3e191c17959693c4b3ea86fa99787ff39b872d4c
clement <clement.cauliez@gmail.com>
2021-12-30 10:19:30 +0100

    fix: [user] removed unnecessary test methods

--- src/test/java/fr/oukilson/backend/entity/UserTest.java
+++ src/test/java/fr/oukilson/backend/entity/UserTest.java
@@ -17,31 +17,5 @@ public class UserTest {
         return new User(2L, "password2", "email2@email.com", "username2");
     }
 
-    @Test
-    public void testAddUserToFriendList() throws Exception {
-        User user1 = user1();
-        User user2 = user2();
-        user1.addUserToFriendList(user2);
-        Assertions.assertEquals(user1.getFriendList().get(0), user2);
-    }
 
-    @Test
-    public void testAddUserToFriendList_Exception() throws Exception {
-        User user1 = user1();
-        User user2 = user2();
-        user1.addUserToFriendList(user2);
-        Assertions.assertThrows(Exception.class, () ->
-                user1.addUserToFriendList(user2));
-    }
-
-    @Test
-    public void testRemoveUserFromFriendList() throws Exception {
-        User user1 = user1();
-        User user2 = user2();
-        User user3 = new User(3L, "password3", "email3@email.com", "nickname3");
-        user1.getFriendList().add(user2);
-        user1.getFriendList().add(user3);
-        user1.removeUserFromFriendList(user2);
-        Assertions.assertEquals(user1.getFriendList().get(0), user3);
-    }
 }
