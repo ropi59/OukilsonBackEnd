5fa1c29b186ac8006c9a7d7939ebb8ad0d94f568
LMika <lemika.pro@gmail.com>
2022-01-10 11:36:55 +0100

    fix[Event]: Add Location repository in Bean EventService

--- src/main/java/fr/oukilson/backend/configuration/EventConfiguration.java
+++ src/main/java/fr/oukilson/backend/configuration/EventConfiguration.java
@@ -2,6 +2,7 @@ package fr.oukilson.backend.configuration;
 
 import fr.oukilson.backend.repository.EventRepository;
 import fr.oukilson.backend.repository.GameRepository;
+import fr.oukilson.backend.repository.LocationRepository;
 import fr.oukilson.backend.repository.UserRepository;
 import fr.oukilson.backend.service.EventService;
 import org.modelmapper.ModelMapper;
@@ -14,7 +15,8 @@ public class EventConfiguration {
     public EventService eventService(EventRepository eventRepo,
                                      UserRepository userRepo,
                                      GameRepository gameRepo,
+                                     LocationRepository locationRepo,
                                      ModelMapper mapper) {
-        return new EventService(eventRepo, userRepo, gameRepo, mapper);
+        return new EventService(eventRepo, userRepo, gameRepo, locationRepo, mapper);
     }
 }
