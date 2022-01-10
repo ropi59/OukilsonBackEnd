9457f75657e75bff5c642f95455777069bf76700
ropi59 <mrseed@hotmail.com>
2022-01-10 17:37:08 +0100

    fix [clean] : delete unused files

--- src/main/java/fr/oukilson/backend/repository/IconRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.oukilson.backend.repository;
-
-import fr.oukilson.backend.entity.Icon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IconRepository extends JpaRepository <Icon, String> {
-}
