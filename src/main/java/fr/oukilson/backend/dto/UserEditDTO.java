cd1e1365a207c201d5f340b69597e3a2247c2e78
ropi59 <mrseed@hotmail.com>
2022-01-10 17:54:59 +0100

    feat [userEditDTO] : delete password attribute, add icon filename attribute

--- src/main/java/fr/oukilson/backend/dto/UserEditDTO.java
+++ src/main/java/fr/oukilson/backend/dto/UserEditDTO.java
@@ -4,16 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserEditDTO {
 
-    private String password;
     private String firstName;
     private String lastName;
     private String email; //TODO test unicité du mail
     private String nickname; //TODO test unicité du nickname
+    private String iconFilename;
 }
