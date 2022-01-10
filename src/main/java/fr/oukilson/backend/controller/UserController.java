d90cd6e55f6679f90c7401022d58a4fb379c79d8
ropi59 <mrseed@hotmail.com>
2022-01-10 17:52:44 +0100

    feat [userController] : delete findall function, add upload icon function and display icon function

--- src/main/java/fr/oukilson/backend/controller/UserController.java
+++ src/main/java/fr/oukilson/backend/controller/UserController.java
@@ -1,14 +1,22 @@
 package fr.oukilson.backend.controller;
 
 import fr.oukilson.backend.dto.UserEditDTO;
-import fr.oukilson.backend.dto.UserListDTO;
+import fr.oukilson.backend.dto.UserNameDTO;
 import fr.oukilson.backend.dto.UserProfilDTO;
+
 import fr.oukilson.backend.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -17,19 +25,11 @@ import java.util.Optional;
 @RequestMapping("users")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private UserService userService;
 
     public UserController(UserService userService){
         this.userService = userService;
-    }
-
-    /**
-     * get list of all users
-     * @return
-     */
-    @GetMapping
-    public List<UserListDTO> findAll(){
-        return this.userService.findAll();
     }
 
     /**
@@ -78,6 +78,44 @@ public class UserController {
     public ResponseEntity<Boolean> delete(@RequestBody UserProfilDTO userProfilDTO){
         this.userService.delete(userProfilDTO);
         return ResponseEntity.ok(true);
+    }
+
+    /**
+     * method to add icon to user profil
+     * @param icon a picture selected by user
+     * @return stock icon in DB
+     */
+    @PostMapping("/uploadFile")
+    public ResponseEntity uploadFile(@RequestBody UserNameDTO userNameDTO, @RequestParam("file") MultipartFile icon){
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.storeFile(userNameDTO, icon));
+
+    }
+
+    /**
+     * display picture in user profil
+     * @param fileName name of the picture
+     * @param request servlet to dld picture
+     * @return the picture saved by user
+     */
+    @GetMapping("/icons/{fileName:.+}")
+    public ResponseEntity<Resource> downloadIcon(@PathVariable String fileName, HttpServletRequest request){
+        Resource resource = userService.loadIconAsResource(fileName);
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException e){
+            logger.info("Couldn't determine file type.");
+        }
+
+        // contentType definition if not exist
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
