7a9e44c96f720608cecdd9fdb5018371dd5079b4
ropi59 <mrseed@hotmail.com>
2022-01-10 17:44:36 +0100

    feat [refacto] : rename as globalConfiguration

--- src/main/java/fr/oukilson/backend/configuration/GlobalConfiguration.java
+++ src/main/java/fr/oukilson/backend/configuration/GlobalConfiguration.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MapperConfiguration {
+public class GlobalConfiguration {
 
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
+
+
 }
