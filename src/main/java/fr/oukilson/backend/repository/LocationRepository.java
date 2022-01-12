b347175c5d4d6625d4c0bf5acfc2f3cfdd6076f4
LMika <lemika.pro@gmail.com>
2022-01-12 17:59:01 +0100

    fix[Location]: Remove unused method.

--- src/main/java/fr/oukilson/backend/repository/LocationRepository.java
+++ src/main/java/fr/oukilson/backend/repository/LocationRepository.java
@@ -2,8 +2,6 @@ package fr.oukilson.backend.repository;
 
 import fr.oukilson.backend.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
-    Optional<Location> findByTownAndZipCodeAndAddress(String town, String zipCode, String address);
 }
