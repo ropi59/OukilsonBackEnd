c591a5ce8d8523a130570536cabe9464e9ba0e64
LMika <lemika.pro@gmail.com>
2022-01-12 16:22:49 +0100

    fix[Event]: Add DTO for sending a game's name alongside its uuid. Use boolean instead of Boolean to avoid deserialization issues.

--- src/main/java/fr/oukilson/backend/dto/event/EventDTO.java
+++ src/main/java/fr/oukilson/backend/dto/event/EventDTO.java
@@ -1,9 +1,8 @@
 package fr.oukilson.backend.dto.event;
 
-import fr.oukilson.backend.dto.game.GameUuidDTO;
+import fr.oukilson.backend.dto.game.GameNameUuidDTO;
 import fr.oukilson.backend.dto.location.LocationDTO;
 import fr.oukilson.backend.dto.user.UserNameDTO;
-import fr.oukilson.backend.entity.Location;
 import lombok.*;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -15,7 +14,7 @@ public class EventDTO {
     private String uuid;
     private String title;
     private UserNameDTO creator;
-    private GameUuidDTO game;
+    private GameNameUuidDTO game;
     private int minPlayer;
     private int maxPlayer;
     private LocalDateTime creationDate;
@@ -23,7 +22,7 @@ public class EventDTO {
     private LocalDateTime endingDate;
     private LocalDateTime limitDate;
     private String description;
-    private Boolean isPrivate;
+    private boolean isPrivate;
     private LocationDTO location;
     // Users registered in the event
     private List<UserNameDTO> registeredUsers = new LinkedList<>();
