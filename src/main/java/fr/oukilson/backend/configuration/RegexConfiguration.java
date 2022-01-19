f029caef2ff4fb6f89b127a69c2aa16a13c59e53
LMika <lemika.pro@gmail.com>
2022-01-19 11:31:26 +0100

    fix[Config]: Make it prettier.

--- src/main/java/fr/oukilson/backend/configuration/RegexConfiguration.java
+++ src/main/java/fr/oukilson/backend/configuration/RegexConfiguration.java
@@ -4,12 +4,10 @@ import fr.oukilson.backend.entity.RegexCollection;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.regex.Pattern;
 
 @Configuration
 public class RegexConfiguration {
-
     @Bean
     public RegexCollection regexCollection(@Value("${environment.emailRegex}") String emailPattern,
                                            @Value("${environment.nicknameRegex}") String nicknamePattern,
@@ -17,5 +15,4 @@ public class RegexConfiguration {
         return new RegexCollection(Pattern.compile(emailPattern),Pattern.compile(nicknamePattern),
                 Pattern.compile(namePattern));
     }
-
 }
