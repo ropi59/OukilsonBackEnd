b1fd00d00e65ac0db3652786879243e88758b9d5
LMika <lemika.pro@gmail.com>
2022-01-19 11:30:01 +0100

    fix[User]: Modify method createUser to return a BadRequest when something is wrong. Modify methods for altering the friend list (route use nickname, not DB id).

--- src/main/java/fr/oukilson/backend/controller/UserController.java
+++ src/main/java/fr/oukilson/backend/controller/UserController.java
@@ -1,80 +1,75 @@
 package fr.oukilson.backend.controller;
 
 import fr.oukilson.backend.dto.UserCreationDTO;
-import fr.oukilson.backend.dto.ResponseDTO;
 import fr.oukilson.backend.dto.UserDTO;
 import fr.oukilson.backend.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/users")
 public class UserController {
-
-    /*
-    declares the beaned service as an attribute then inject it inside the constructor
-     */
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
-    public ResponseEntity<List<UserDTO>> findAllUsers(){
-        return ResponseEntity.ok(this.userService.findAll());
-    }
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> findUser(@PathVariable Long id){
-        return ResponseEntity.ok(this.userService.findById(id));
-    }
     /**
-     * method to save a user in the database
+     * Method to save a user in the database
      * @param userCreationDTO the user object to be saved
-     * @return a response entity validating the created user
+     * @return ResponseEntity<UserDTO>
      */
     @PostMapping()
-    public ResponseEntity<ResponseDTO> createUser(@Valid @RequestBody UserCreationDTO userCreationDTO){
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.userService.createUser(userCreationDTO));
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserCreationDTO userCreationDTO){
+        ResponseEntity<UserDTO> result;
+        try {
+            UserDTO event = this.userService.createUser(userCreationDTO);
+            if (event!=null)
+                result = ResponseEntity.status(HttpStatus.CREATED).body(event);
+            else
+                result = ResponseEntity.badRequest().build();
+        }
+        catch(Exception e) {
+            result = ResponseEntity.badRequest().build();
+        }
+        return result;
     }
 
     /**
-     * queries the service to add a user to another user's friend list, asking
-     * for both users' ids
-     * @param id1 Long, id of the user whose list we wish to modify
-     * @param id2 Long, id of the user we wish to add to the list
-     * @return a response entity
+     * Add a user to another user's friend list, asking for both users' nickname
+     * @param nickname1 Nickname of the main user
+     * @param nickname2 Nickname of the friend to add
+     * @return ResponseEntity<Boolean>
      */
     @PutMapping("/add/{id1}/{id2}")
-    public ResponseEntity<ResponseDTO> addUserToFriendList(@PathVariable Long id1,
-                                                           @PathVariable Long id2) {
-        return ResponseEntity.ok(this.userService.addUserToFriendList(id1, id2));
+    public ResponseEntity<Boolean> addUserToFriendList(@PathVariable(name = "id1") String nickname1,
+                                                       @PathVariable(name = "id2") String nickname2) {
+        return ResponseEntity.ok(this.userService.addUserToFriendList(nickname1, nickname2));
     }
 
     /**
-     * removes a user from another user's friend list
-     * @param id1 Long, id of the user whose list we wish to modify
-     * @param id2 Long, id of the user we wish to remove from the list
-     * @return a response entity
+     * Remove a user from another user's friend list
+     * @param nickname1 Nickname of the main user
+     * @param nickname2 Nickname of the friend to remove
+     * @return ResponseEntity<Boolean>
      */
     @PutMapping("/remove/{id1}/{id2}")
-    public ResponseEntity<ResponseDTO> removeUserFromFriendList(@PathVariable Long id1,
-                                                                @PathVariable Long id2) {
-        return ResponseEntity.ok(this.userService.removeUserFromFriendList(id1, id2));
+    public ResponseEntity<Boolean> removeUserFromFriendList(@PathVariable(name = "id1") String nickname1,
+                                                                @PathVariable(name = "id2") String nickname2) {
+        return ResponseEntity.ok(this.userService.removeUserFromFriendList(nickname1, nickname2));
     }
 
     /**
-     * empties a user's friend list
-     * @param id Long, id of the user whose list we wish to empty
-     * @return a response entity
+     * Empties a user's friend list
+     * @param nickname User's nickname
+     * @return ResponseEntity<Boolean>
      */
     @PutMapping("/empty/{id}")
-    public ResponseEntity<ResponseDTO> emptyFriendList(@PathVariable Long id) {
-        return ResponseEntity.ok(this.userService.emptyFriendList(id));
+    public ResponseEntity<Boolean> emptyFriendList(@PathVariable(name = "id") String nickname) {
+        return ResponseEntity.ok(this.userService.emptyFriendList(nickname));
     }
 }
