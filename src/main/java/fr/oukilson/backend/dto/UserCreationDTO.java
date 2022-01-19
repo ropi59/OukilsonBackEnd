77ea668d0bbfdd34771755f26dff95aef46078f3
LMika <lemika.pro@gmail.com>
2022-01-19 11:33:16 +0100

    fix[User]: Make it prettier.

--- src/main/java/fr/oukilson/backend/dto/UserCreationDTO.java
+++ src/main/java/fr/oukilson/backend/dto/UserCreationDTO.java
@@ -1,22 +1,16 @@
 package fr.oukilson.backend.dto;
 
-
 import lombok.*;
-
 import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserCreationDTO {
-
     @NotNull
     private String password;
     @NotNull
     private String email;
     @NotNull
     private String nickname;
-
-
-
 }
