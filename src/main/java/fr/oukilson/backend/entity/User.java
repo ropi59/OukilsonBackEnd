72f958be5392870b2d80af81aa089f9f5d980e08
LMika <lemika.pro@gmail.com>
2022-01-24 08:36:48 +0100

    feat[User] : Add other attributes defined by the corresponding table schema

--- src/main/java/fr/oukilson/backend/entity/User.java
+++ src/main/java/fr/oukilson/backend/entity/User.java
@@ -1,28 +1,52 @@
 package fr.oukilson.backend.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "user")
-@Data
+@Table(name="user")
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-
     @Id
-    @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String password;
-    private String firstName;
-    private String lastName;
-    private String email; //TODO test unicité du mail
-    private String nickname; //TODO test unicité du nickname
-    @Column(name="icon_filename")
-    private String iconFilename;
+    private Long id;                    // DB id
+    private String nickname;            // Unique username; also used to access from the client
+    private String password;            // Encrypted version of the password
+    private String email;               // Email of the user
+    @Column(name = "first_name")
+    private String firstName;           // First name of the user
+    @Column(name = "last_name")
+    private String lastName;            // Last name of the user
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "friend_list",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "friend_id"))
+    private List<User> friendList = new ArrayList<>();
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinTable(name = "denied_list",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "denied_id"))
+    private List<User> deniedList = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
 }
