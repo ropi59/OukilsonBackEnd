affd5326ecf01452dccaab783a389fe79dca7d2e
LMika <lemika.pro@gmail.com>
2022-01-24 08:27:55 +0100

    fix[Project] : Remove unused files

--- src/main/java/fr/oukilson/backend/dto/LocationDTO.java
+++
@@ -1,8 +0,0 @@
-package fr.oukilson.backend.dto;
-
-public class LocationDTO {
-
-    private String town;
-    private String zip_code;
-    private String adress;
-}
--- src/main/java/fr/oukilson/backend/repository/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.oukilson.backend.repository;
-
-import fr.oukilson.backend.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository <Event, Long> {
-
-}
