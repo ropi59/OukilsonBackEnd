b30cbca4ad769a6ec642ba1362abc3d7fd766684
LMika <lemika.pro@gmail.com>
2022-01-24 08:39:41 +0100

    fix[User] : Change bean to comply with the new constructor for the service

--- src/main/java/fr/oukilson/backend/configuration/UserConfiguration.java
+++ src/main/java/fr/oukilson/backend/configuration/UserConfiguration.java
@@ -1,19 +1,17 @@
 package fr.oukilson.backend.configuration;
 
+import fr.oukilson.backend.model.RegexCollection;
 import fr.oukilson.backend.repository.UserRepository;
 import fr.oukilson.backend.service.UserService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UserConfiguration {
-
     @Bean
-    public UserService userService(UserRepository userRepository, ModelMapper mapper,
-                                   @Value("${file.upload-dir}")String path){
-        return new UserService(userRepository, mapper, path);
+    public UserService userService(UserRepository userRepository, ModelMapper modelMapper,
+                                   RegexCollection regexCollection){
+        return new UserService(userRepository, modelMapper, regexCollection);
     }
-
 }
